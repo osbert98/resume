17f0d2a6e7bcb8fd8bc29bb4849708679a2d47d4
osber <osber@DESKTOP-789N9AL.realtek>
2020-10-28 12:41:01 +0800

    *first commit

---
+++ newresume/src/newresume/workskills.java
@@ -0,0 +1,12 @@
+package newresume;
+
+public class workskills {
+
+	String workskills;
+	
+	
+	workskills(){
+		
+		this.workskills="Microsoft office = 100% ,\n  	    Java Programming = 25%";
+	}
+}
